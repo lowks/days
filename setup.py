20046bb4d425a409bff89521059fcf95891e4008
Jarvys <yuhan534@126.com>
2014-04-26 20:57:34 +0800

    add README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='days',
-	version='0.0.1',
+	version='0.0.2',
 	description='shortcuts to get today, yesterday & tommorrow',
 	author='jarvys',
 	author_email='yuhan534@126.com',
